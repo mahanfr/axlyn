e138f794934fe4b5d44efe83ec61633bfaef9006
mahan farzaneh <mahan.farzaneh@gmail.com>
2022-09-09 13:29:36 +0430

    hosting documents and static files for admin server

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use http_body::Full;
+use hyper::body::Bytes;
 use hyper::server::conn::Http;
 use hyper::service::service_fn;
 use hyper::{Body, Method, Request, Response, StatusCode};
 use std::convert::Infallible;
-use std::fs::File;
+use std::error::Error;
+use std::fs::{File};
 use std::io::{BufRead, BufReader};
 use std::net::SocketAddr;
 use tokio::join;
@@ -46,8 +49,44 @@ async fn index1(req: Request<Body>) -> Result<Response<Body>, Infallible> {
         .unwrap())
 }
 
-async fn index2(_: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("Admin server".into()))
+async fn index2(req: Request<Body>) -> Result<Response<Full<Bytes>>, Infallible> {
+    if req.uri().path() == "/" && req.method() == Method::GET {
+        return Ok(
+            get_file("resources/index.html").await.unwrap()
+        )
+    }else if req.uri().path().starts_with("/static") && req.method() == Method::GET {
+        return Ok(
+            get_file(format!("resources{}",req.uri().path()).as_str()).await.unwrap()
+        )
+    }else if req.uri().path() == "/favicon.ico" && req.method() == Method::GET {
+        return Ok(
+            get_file("resources/favicon.ico").await.unwrap()
+        )
+    }
+    Ok(not_found().await)
+}
+
+async fn get_file(filename: &str) -> Result<Response<Full<Bytes>>,Box<dyn Error>> {
+    if let Ok(contents) = tokio::fs::read(filename).await {
+        let body = contents.into();
+        return Ok(Response::new(Full::new(body)));
+    }
+
+    Ok(not_found().await)
+}
+
+async fn not_found() -> Response<Full<Bytes>>{
+    if let Ok(contents) = tokio::fs::read("resources/404.html").await {
+        let body = contents.into();
+        return Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(Full::new(body))
+        .unwrap()
+    }
+    Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(Full::new("not found".into()))
+        .unwrap()
 }
 
 fn read_path_config(
