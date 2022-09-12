176dc1144a92bb7abc93850debcc26dfac83ea91
mahan farzaneh <mahan.farzaneh@gmail.com>
2022-09-13 02:38:48 +0430

    admin web gui design change and refactoring

--- utils/admin-interface/src/App.js
+++ utils/admin-interface/src/App.js
@@ -1,34 +1,17 @@
 import React from "react";
 import Navbar from "./Components/global/Navbar";
-import Sidebar from "./Components/global/Sidebar";
-import SidebarItem from "./Components/global/SidebarItem";
-import { faCog, faDatabase, faNetworkWired, faTemperatureThreeQuarters, faCloudArrowUp, faPlugCirclePlus, faUsers } from "@fortawesome/free-solid-svg-icons";
 import EndPointView from "./Components/EndPointView";
 import DatabaseView from "./Components/DatabaseView";
 
 function App() {
-  const [sidebar, setSidebar] = React.useState(true);
-  const [activeWindow, setActiveWindow] = React.useState("status");
-
-  const setWindow = (name) => {
-    setActiveWindow(name);
-  };
+  const [activeWindow, setActiveWindow] = React.useState("dashboard");
 
   return (
     <>
-      <Navbar isSidebarOpen={sidebar} onMenuClick={() => { setSidebar(!sidebar) }} />
+      <Navbar 
+        active={activeWindow} 
+        onSelect={(selection) => { setActiveWindow(selection) }} />
       <div style={{ position: 'fixed', width: '100%', height: "100%", display: 'flex', flexDirection: 'row' }}>
-        {sidebar &&
-          <Sidebar>
-            <SidebarItem icon={faTemperatureThreeQuarters} name="Status" active={activeWindow === "status"} onClick={() => setWindow("status")} />
-            <SidebarItem icon={faDatabase} name="Database" active={activeWindow === "database"} onClick={() => setWindow("database")} />
-            <SidebarItem icon={faNetworkWired} name="Endpoints" active={activeWindow === "endpoints"} onClick={() => setWindow("endpoints")} />
-            <SidebarItem icon={faCloudArrowUp} name="Backup" active={activeWindow === "backup"} onClick={() => setWindow("backup")} />
-            <SidebarItem icon={faUsers} name="Users" active={activeWindow === "users"} onClick={() => setWindow("users")} />
-            <SidebarItem icon={faCog} name="Settings" active={activeWindow === "settings"} onClick={() => setWindow("settings")} />
-            <SidebarItem icon={faPlugCirclePlus} name="Plugins" active={activeWindow === "plugins"} onClick={() => setWindow("plugins")} />
-          </Sidebar>
-        }
         {activeWindow === "endpoints" && 
           <EndPointView />
         }
--- utils/admin-interface/src/Components/global/Navbar.js
+++ utils/admin-interface/src/Components/global/Navbar.js
@@ -1,21 +1,59 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faCircleInfo, faFileLines, faRightFromBracket, faTerminal } from '@fortawesome/free-solid-svg-icons';
 import styles from './navbar.module.css';
-import Banner from '../../svgs/Banner.svg'
+import Banner from '../../svgs/Axlyn.svg'
 
 const Navbar = (props) => {
-
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.navbar_left}>
-        <FontAwesomeIcon onClick={() => props.onMenuClick()} className={`${styles.menu} ${props.isSidebarOpen && styles.menu_selected}`} size='xl' icon={faBars} />
-        <a className={styles.logo} href="/">
-          <img src={Banner} alt="axlyn" width="90px" />
-        </a>
+    <nav>
+      <div className={styles.navbar}>
+        <div className={styles.navbar_left}>
+          <a className={styles.logo} href="/">
+            <img src={Banner} alt="axlyn" width="90px" />
+          </a>
+        </div>
+        <div className={styles.navbar_right}>
+          <h3 className={styles.version}>Version | 0.1.1</h3>
+        </div>
       </div>
-      <div className={styles.navbar_right}>
-        <a className={styles.logout} href="/logout">logout</a>
+      <div className={styles.navbar}>
+        <div className={styles.navbar_left}>
+          <a 
+            className={`${styles.navLinks} ${props.active === "dashboard" && styles.navLinks_selected}`} 
+            onClick={()=>props.onSelect("dashboard")}
+            href="#Dashboard">Dashboard</a>
+          <a 
+            className={`${styles.navLinks} ${props.active === "database" && styles.navLinks_selected}`} 
+            onClick={()=>props.onSelect("database")}
+            href="#Database">Database</a>
+          <a 
+            className={`${styles.navLinks} ${props.active === "endpoints" && styles.navLinks_selected}`} 
+            onClick={()=>props.onSelect("endpoints")}
+            href="#Endpoints">Endpoints</a>
+          <a 
+            className={`${styles.navLinks} ${props.active === "backup" && styles.navLinks_selected}`} 
+            onClick={()=>props.onSelect("backup")}
+            href="#Backup">Backup</a>
+          <a 
+            className={`${styles.navLinks} ${props.active === "users" && styles.navLinks_selected}`} 
+            onClick={()=>props.onSelect("users")}
+            href="#Users">Users</a>
+          <a 
+            className={`${styles.navLinks} ${props.active === "settings" && styles.navLinks_selected}`} 
+            onClick={()=>props.onSelect("settings")}
+            href="#Settings">Settings</a>
+          <a 
+            className={`${styles.navLinks} ${props.active === "plugin" && styles.navLinks_selected}`} 
+            onClick={()=>props.onSelect("plugin")}
+            href="#Plugin">Plugin</a>
+        </div>
+        <div className={styles.navbar_right}>
+          <FontAwesomeIcon className={styles.icon} icon={faRightFromBracket} />
+          <FontAwesomeIcon className={styles.icon} icon={faTerminal} />
+          <FontAwesomeIcon className={styles.icon} icon={faCircleInfo} />
+          <FontAwesomeIcon className={styles.icon} icon={faFileLines} />
+        </div>
       </div>
     </nav>
   )
